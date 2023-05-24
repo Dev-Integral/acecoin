f3f044e1876c12ad9bfa54b8277729fe24647ade
Taiwo Ayoola <tayoola13@yahoo.com>
2023-05-24 18:24:30 +0100

    Completed the project dependencies set up and installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper grid grid-cols-12">
+      <div className='col-span-12 md:col-span-8'></div>
+      <div className='col-span-12 md:col-span-4'></div>
     </div>
   );
 }
